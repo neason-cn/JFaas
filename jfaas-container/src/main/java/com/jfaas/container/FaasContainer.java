8745e7a0eea5fe7a092fd4d397bda174bf4952c0
neason-cn <cqwjsl123@126.com>
2021-10-28 21:23:04 +0800

    init container

---
+++ jfaas-container/src/main/java/com/jfaas/container/FaasContainer.java
@@ -0,0 +1,13 @@
+package com.jfaas.container;
+
+/**
+ * @author : neason-cn
+ * @date : 2021/10/28
+ */
+public class FaasContainer {
+
+    public static void main(String[] args) {
+        System.out.println("hello jfaas");
+    }
+
+}
